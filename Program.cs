a72c365d6710237b20d3ecaa1404311b97b26e25
Paulo Kinjo <paulokinjo@hotmail.com>
2024-07-04 19:14:59 -0400

    Testing my arrays

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿public class InitArray
+{
+    public static void Main(string[] args)
+    {
+        int[] array; // declare array named array
+
+        // create the space for array and initialize to default zeros
+        array = new int[5]; // 5 int elements
+
+        Console.WriteLine("{0}{1,8}", "Index", "Value"); // headings
+
+        // output each array element's value
+        for (int counter = 0; counter < array.Length; ++counter)
+            Console.WriteLine("{0,5}{1,8}", counter, array[counter]);
+    } // end Main
+} // end class InitArray
